4e98446b0a2b715b632532b53cda8fd720cf622b
Liao Xueqing <xueqingliao@foxmail.com>
2019-06-05 14:13:44 +0800

    Add getTodoById in todo service

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -13,4 +13,8 @@ export class TodoService {
   getTodos(): Observable<Todo[]> {
     return of(TODOS);
   }
+
+  getTodoById(id): Observable<Todo> {
+    return of(TODOS.find(todo => todo.id === id));
+  }
 }
