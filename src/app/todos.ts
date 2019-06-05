0bdded2c887ee7cf4330db5555d561be0f44246d
Liao Xueqing <xueqingliao@foxmail.com>
2019-06-05 13:46:00 +0800

    Create Todos mock-data

---
+++ src/app/todos.ts
@@ -0,0 +1,21 @@
+import { Todo } from './todo';
+
+export const TODOS: Todo[] = [
+  {
+    id: 1,
+    content: 'homework',
+    isChecked: false,
+  }, {
+    id: 2,
+    content: 'clean floor',
+    isChecked: false,
+  }, {
+    id: 3,
+    content: 'movie',
+    isChecked: false,
+  }, {
+    id: 4,
+    content: 'listen music',
+    isChecked: false,
+  }
+];
