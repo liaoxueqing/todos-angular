22983067590e10cdb401a2f3be49e76a9aa265b0
Liao Xueqing <xueqingliao@foxmail.com>
2019-06-05 15:55:01 +0800

    Config route for todo add component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {TodoListComponent} from './todo-list/todo-list.component';
 import {TodoDetailComponent} from './todo-detail/todo-detail.component';
+import {TodoAddComponent} from './todo-add/todo-add.component';
 
 const routes: Routes = [
   { path: '', component: TodoListComponent },
   { path: 'detail/:id', component: TodoDetailComponent },
+  { path: 'add', component: TodoAddComponent },
 ];
 
 @NgModule({
