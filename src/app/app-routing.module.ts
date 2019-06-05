2f0c170aaacb255cec02cb814069c45284bd29a4
Liao Xueqing <xueqingliao@foxmail.com>
2019-06-05 14:01:07 +0800

    Use ng generate component todo-detail to create Todo detail component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {TodoListComponent} from './todo-list/todo-list.component';
+import {TodoDetailComponent} from './todo-detail/todo-detail.component';
 
 const routes: Routes = [
   { path: '', component: TodoListComponent },
+  { path: 'detail/:id', component: TodoDetailComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoDetailComponent } from './todo-detail/todo-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoListComponent
+    TodoListComponent,
+    TodoDetailComponent
   ],
   imports: [
     BrowserModule,
